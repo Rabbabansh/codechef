9137043e891625a65ab35ce0dfbc422329c3b01f
Ansh Babbar <ansh28babbar@gmail.com>
2021-10-11 16:14:07 +0530

    'update'

--- OCTLONG21/DIGITREM.py
+++ OCTLONG21/DIGITREM.py
@@ -1,18 +1,18 @@
 for t in range(int(input())):
-    num, X = map(int, input().split())
+    num, x = map(int, input().split())
     num_list = list(str(num))
 
-    if str(X) not in num_list:
+    if str(x) not in num_list:
         print(0)
     elif len(num_list) == 1:
         print(1)
-    elif X == 0:
+    elif x == 0:
         first = 0
         rem = 0
         for i in range(len(num_list)):
-            if int(num_list[i]) == X:
+            if int(num_list[i]) == x:
                 first = len(num_list) - i - 1
-                rem = int(str(num)[i + 1 :])
+                rem = int(str(num)[(i + 1) :])
                 break
         bruh = 10 ** first - rem
         bruh += int("1" * first)
@@ -21,8 +21,8 @@ for t in range(int(input())):
         first = 0
         rem = 0
         for i in range(len(num_list)):
-            if int(num_list[i]) == X:
+            if int(num_list[i]) == x:
                 first = len(num_list) - i - 1
-                rem = int(str(num)[i + 1 :])
+                rem = int(str(num)[(i + 1) :])
                 break
         print(10 ** first - rem)
